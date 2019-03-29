abfc92ebc53a24801da860d6870fd238fec702fe
Davide Faconti <davide.faconti@eurecat.org>
2019-03-29 11:50:07 +0100

    two libraries unified....

---
+++ include/grid_map/grid_map_core.hpp
@@ -0,0 +1,18 @@
+/*
+ * grid_map_core.hpp
+ *
+ *  Created on: Jul 14, 2014
+ *      Author: Péter Fankhauser
+ *	 Institute: ETH Zurich, ANYbotics
+ */
+
+#pragma once
+
+#include "grid_map/TypeDefs.hpp"
+#include "grid_map/GridMap.hpp"
+#include "grid_map/SubmapGeometry.hpp"
+#include "grid_map/GridMapMath.hpp"
+#include "grid_map/BufferRegion.hpp"
+#include "grid_map/Polygon.hpp"
+#include "grid_map/iterators/iterators.hpp"
+#include "grid_map/eigen_plugins/Functors.hpp"
---
+++ include/grid_map/iterators/iterators.hpp
@@ -0,0 +1,18 @@
+/*
+ * iterators.hpp
+ *
+ *  Created on: Sep 22, 2014
+ *      Author: Péter Fankhauser
+ *   Institute: ETH Zurich, ANYbotics
+ */
+
+#pragma once
+
+#include "grid_map/iterators/GridMapIterator.hpp"
+#include "grid_map/iterators/SubmapIterator.hpp"
+#include "grid_map/iterators/CircleIterator.hpp"
+#include "grid_map/iterators/EllipseIterator.hpp"
+#include "grid_map/iterators/SpiralIterator.hpp"
+#include "grid_map/iterators/LineIterator.hpp"
+#include "grid_map/iterators/PolygonIterator.hpp"
+#include "grid_map/iterators/SlidingWindowIterator.hpp"
